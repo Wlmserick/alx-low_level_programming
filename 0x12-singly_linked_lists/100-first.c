cd1aa5a9c0549fcadd6f423b7dc5e6bb49ca6fe9
Wlmserick <erickwlms@gmail.com>
2023-10-27 12:37:40 -0700

    C - Singly linked lists

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * dmain - function executed
+ * Return: no return.
+ */
+
+void __attribute__ ((constructor)) dmain()
+{
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
+}
